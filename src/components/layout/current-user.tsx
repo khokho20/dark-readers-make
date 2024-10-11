781e55733388affe2d1f605123b490a9b393de13
khokho <michealkosisochukwu300@gmail.com>
2024-10-11 20:29:18 +0100

    all

--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -1,16 +1,13 @@
+import { Popover } from 'antd'
 import React from 'react'
 
 const CurrentUser = () => {
   return (
     <div>
-      <Popover 
-       placement ='bottomRight'
-       trigger="click"
-       overlayInnerStyle={( padding: 0 )}
-       overlayStyle={(zIndex: 999)}
-   >
-        TEST
-      </Popover>
+    <Popover>
+        
+        
+    </Popover>
     </div>
   )
 }
