d4e44fec2bf375152e138bb5bcab8d27f495ede6
khokho <michealkosisochukwu300@gmail.com>
2024-10-23 06:57:04 +0100

    staged

--- src/Providers/Data/index.tsx
+++ src/Providers/Data/index.tsx
@@ -17,7 +17,6 @@ export const client = new GraphQLClient(API_URL, {
         }
     }
 })
-
 export  const wsClient = typeof window !== "undefined" 
   ? createClient({
     url: WS_URL,
@@ -32,6 +31,6 @@ export  const wsClient = typeof window !== "undefined"
     }
   })
   : undefined
-
+  
   export const dataProvider = graphqlDataProvider(client);
   export const liveProvider = wsClient? graphqlLiveProvider(wsClient) : undefined;
